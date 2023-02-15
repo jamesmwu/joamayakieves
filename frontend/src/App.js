63f35e79a31e3e9c24f5bec9ed474446c5f411eb
Aki Hasegawa-Johnson <104283523+aki0501@users.noreply.github.com>
2023-02-14 22:21:20 -0800

    Added navigation between the Home page and Profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     <div className='App'>
       <p>HI</p>
+      <Routes>
+        <Route path='' element={<Home />}/>
+        <Route path='/profile' element={<Profile />}/>
+      </Routes>
     </div>
   );
 }
