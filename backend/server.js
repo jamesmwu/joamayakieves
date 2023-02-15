3dd182e2a8dc7ca9a703a931954ccd5472e9a612
jameswu <jamesw0462@gmail.com>
2023-02-14 22:48:28 -0800

    Created get, post, delete endpoints for users and Users Schema

--- backend/server.js
+++ backend/server.js
@@ -18,3 +18,39 @@ mongoose
   .catch(console.error);
 
 app.listen(3001, () => console.log('Server listening on port 3001'));
+
+const User = require('./models/users');
+
+//Users endpoints
+app.get('/users', async (req, res) => {
+  const userList = await User.find();
+
+  res.json(userList);
+});
+
+app.post('/users/create', async (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  await user.save();
+
+  res.json(user);
+});
+
+app.delete('/users/delete/:_id', async (req, res) => {
+  const result = await User.findByIdAndDelete(req.params._id);
+
+  res.json(result);
+});
+
+// app.put('/users/edit/:_id', async (req, res) => {
+//   const user = await User.findById(req.params._id);
+
+//   user.username = req.body.username;
+//   user.password = req.body.password;
+//   user.save();
+
+//   res.json(user);
+// });
