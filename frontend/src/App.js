e31acc10117795b7b1df289bbdd12020c0330cb1
Joana Fang <joannafong123@gmail.com>
2023-02-16 13:09:17 -0800

    Added navigation between home and profile pages
    *Added navigation bar
    *Added simple styling
    *Changed so will only refresh relevant sections of page when navigation bar buttons are clicked

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
+import Navbar from "./Navbar"
 
-function App() {
+function App(){
   return (
+    <>
+    <Navbar/>
     <div className='App'>
-      <p>HI</p>
       <Routes>
-        <Route path='' element={<Home />}/>
+        <Route path='/home' element={<Home />}/>
         <Route path='/profile' element={<Profile />}/>
       </Routes>
     </div>
+  </>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
+import "./styles/Navbar.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
