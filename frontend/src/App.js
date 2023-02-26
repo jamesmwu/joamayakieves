d11509e34a6db7092813ce11cf97659cdd0f205e
Maya Balakrishnan <mayabalakrish@gmail.com>
2023-02-26 12:57:49 -0800

    Added unstyled New Post button and popup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import Navbar from "./Navbar"
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 function App(){
   return (
@@ -14,7 +16,30 @@ function App(){
         <Route path='/profile' element={<Profile />}/>
       </Routes>
     </div>
+    
+    <Popup trigger={
+      <button> New Post </button>
+    }
+    modal nested>
+      {
+        close => (
+          <div className='modal'>
+            <div className='content'>
+              New Post
+            </div>
+            <div>
+              <button onClick={
+                () => close()
+              }>
+                Close
+              </button>
+            </div>
+          </div>
+        )
+      }
+    </Popup>
   </div>
+
   );
 }
 
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,8 @@ import React from "react";
 
 export default function Home() {
     return (
+        
         <p>HOMIE</p>
+        
     );
 }
