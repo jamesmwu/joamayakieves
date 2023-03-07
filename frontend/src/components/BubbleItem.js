a2a41d7a523252aa2c122706923789a8040987e1
Aki Hasegawa-Johnson <104283523+aki0501@users.noreply.github.com>
2023-03-06 21:19:21 -0800

    Made link optional

--- frontend/src/components/BubbleItem.js
+++ frontend/src/components/BubbleItem.js
@@ -5,7 +5,7 @@ import UnlikedHeart from "../icons/unliked-heart.jsx";
 // import User from "../icons/user.jsx"
 // import Arrow from "../icons/arrow-up-right.jsx"
 
-function BubbleItem({ title, author, about }) {
+function BubbleItem({ title, author, about, link }) {
   const [isUnlikedHeart, setUnlikedHeart] = useState(true);
 
   function handleHeartClick() {
@@ -28,6 +28,20 @@ function BubbleItem({ title, author, about }) {
           )}
         </div>
       </div>
+      <div className="heart" onClick={handleHeartClick}>
+        {isUnlikedHeart ? (
+          <UnlikedHeart />
+        ) : (
+          <LikedHeart />
+        )}
+      </div>
+
+      {link ?
+        <div>
+          <a href={link} target="_blank" rel="noopener noreferrer">
+            <button>{link}</button>
+          </a>
+        </div> : null}
     </>
   );
 }
