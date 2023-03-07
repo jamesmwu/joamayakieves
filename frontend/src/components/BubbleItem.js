ab69a3dcb729fc4cd33ca5bd6b432129235c426c
jameswu <jamesw0462@gmail.com>
2023-03-06 20:47:03 -0800

    Mapped BubbleItems (posts) with boilerplate title to display feed

--- frontend/src/components/BubbleItem.js
+++ frontend/src/components/BubbleItem.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import "../styles/Bubbles.css"
-import LikedHeart from "../icons/liked-heart.jsx"
-import UnlikedHeart from "../icons/unliked-heart.jsx"
-import User from "../icons/user.jsx"
-import Arrow from "../icons/arrow-up-right.jsx"
+import "../styles/Bubbles.css";
+import LikedHeart from "../icons/liked-heart.jsx";
+import UnlikedHeart from "../icons/unliked-heart.jsx";
+// import User from "../icons/user.jsx"
+// import Arrow from "../icons/arrow-up-right.jsx"
 
-function BubbleItem({title, author, about}) {
-  const [ isUnlikedHeart, setUnlikedHeart ] = useState(true);
+function BubbleItem({ title, author, about }) {
+  const [isUnlikedHeart, setUnlikedHeart] = useState(true);
 
   function handleHeartClick() {
     setUnlikedHeart(!isUnlikedHeart);
@@ -14,22 +14,22 @@ function BubbleItem({title, author, about}) {
 
   return (
     <>
-    <div className="bubble">
-      <div className="text">
-        <div className="title">{title}</div>
-        <div className="author">{author}</div>
-        <div className="about">{about}</div>
+      <div className="bubble">
+        <div className="text">
+          <div className="title">{title}</div>
+          <div className="author">{author}</div>
+          <div className="about">{about}</div>
+        </div>
+        <div className="heart" onClick={handleHeartClick}>
+          {isUnlikedHeart ? (
+            <UnlikedHeart />
+          ) : (
+            <LikedHeart />
+          )}
+        </div>
       </div>
-      <div className="heart" onClick={handleHeartClick}>
-        {isUnlikedHeart ? (
-          <UnlikedHeart />
-        ) : (
-          <LikedHeart />
-        )}
-      </div>
-    </div>
     </>
-  )
+  );
 }
 
 export default BubbleItem;
