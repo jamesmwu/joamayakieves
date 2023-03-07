478b573cbd7ef11b9dfa34a1cce92e71763dc489
Joana Fang <joannafong123@gmail.com>
2023-03-06 21:43:21 -0800

    Add non-functional login feature and styling to profile page
    Add placeholder for profile picture
    Add placeholder for user information
    Add login feature using username and password

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Profile from './pages/Profile';
+//import Profile from './pages/Profile';
 import Navbar from "./Navbar"
+import {Login} from './pages/Profile';
 
 function App(){
+  //const [currentForm, setCurrentForm] = useState('login'); // do i need
   return (
     <div>
     <Navbar/>
     <div className='App'>
       <Routes>
         <Route path='' element={<Home />}/>
-        <Route path='/profile' element={<Profile />}/>
+        <Route path='/profile' element={<Login />}/>
       </Routes>
     </div>
   </div>
--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,7 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
+import '../styles/Profile.css'
 
+export const Login = () =>{
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleSubmit = (e) => { // pass in e to get state
+        e.preventDefault(); // otherwise, page will be reloaded and we will lose input
+        console.log(email); // have access bc state
+    }
+    return(
+        <div className="pagestyle">
+        <div className="loginstyle">
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="email">email: </label>
+            <div className="Textboxstyle">
+            <input value={email} onChange={(e)=> setEmail(e.target.value)} type="email" placeholder="emailname@gmail.com" id="email" name="email"/>
+           </div>
+            <label htmlFor="password">password: </label>
+            <div className="Textboxstyle">
+            <input value={pass} onChange={(e)=> setPass(e.target.value)}type="password" placeholder="*********" id="password" name="password"/>
+            </div>
+            <button>Log In</button>
+        </form>
+        <button>Don't have an account? Click here to register</button>
+        </div>
+        <p>
+
+        </p>
+        <img src="/jwac_pic.png" alt=""/>
+        <p>
+            User: Joe Bruin
+        </p>
+        <p>
+        Email: joebruin@gmail.com
+        </p>
+        <ul>
+            <li>QWERHacks</li>
+            <li>Alumni Networking Night</li>
+            <li>HOTH</li>
+        </ul>
+
+        </div>
+    )
+}
+
+/*
 export default function Profile() {
     return (
         <p>PROFILE</p>
     );
-}
+  
+} */
