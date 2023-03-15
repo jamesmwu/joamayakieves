c2ca19cc3c68c7041f32e1179d6cfbd936f60e58
jameswu <jamesw0462@gmail.com>
2023-03-15 15:47:19 -0700

    Minor edits to remove warnings

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -29,26 +29,29 @@ function Profile() {
   const userDetails = useAuthState();
 
   const navigate = useNavigate();
-      const dispatch = useAuthDispatch();
-      const handleLogout = () => {
-          logout(dispatch);
-          navigate('/login');
-          return;
-      };
+  const dispatch = useAuthDispatch();
+  const handleLogout = () => {
+    logout(dispatch);
+    navigate('/login');
+    return;
+  };
 
   return (
     <div>
-        <h1>What's Poppin {userDetails.username}</h1>
-        <button onClick={handleLogout} className='button'>
-            Logout    
-        </button>
-        <div className="bubble-container">
-            {userBubbles.map((bub) => {
-                if (bub.user==userDetails.username) {
-                    return (<BubbleItem key={bub._id} title={bub.title} author={bub.user} link={bub.link} about={bub.content} />);
-                }
-            }).reverse()}
-        </div>
+      <h1>What's poppin, {userDetails.username}</h1>
+      <h2>Your posts</h2>
+      <div className="bubble-container">
+        {userBubbles.map((bub) => {
+          if (bub.user === userDetails.username) {
+            return (<BubbleItem key={bub._id} title={bub.title} author={bub.user} link={bub.link} about={bub.content} />);
+          }
+          else return null;
+        }).reverse()}
+      </div>
+
+      <button onClick={handleLogout} className='button'>
+        Logout
+      </button>
     </div>
   );
 }
