9f646e1e7f7632fba59c517079e2e56b443cc1f0
jameswu <jamesw0462@gmail.com>
2023-03-16 20:28:35 -0700

    Created deleting feature for posts that you've created

--- frontend/src/components/BubbleItem.js
+++ frontend/src/components/BubbleItem.js
@@ -3,25 +3,27 @@ import "../styles/Bubbles.css";
 import LikedHeart from "../icons/liked-heart.jsx";
 // import UnlikedHeart from "../icons/unliked-heart.jsx";
 import axios from 'axios';
+import Trash from '../icons/Trash-Icon.png';
 
 // import User from "../icons/user.jsx"
 // import Arrow from "../icons/arrow-up-right.jsx"
 
 const URL = 'http://localhost:3001';
 
-function BubbleItem({ postId, title, author, about, link, likes }) {
-  // const [isUnlikedHeart, setUnlikedHeart] = useState(true);
+function BubbleItem({ postId, title, author, about, link, likes, profile }) {
 
-  // const [likeCount, setLikes] = useState([]);
 
   async function updateLikes() {
     axios
       .put(URL + '/posts/likes/' + postId)
-      .then(function (response) {
-      })
       .catch(function (error) {
         console.log(error);
       });
+  }
+
+  async function deletePost() {
+    axios.delete(URL + '/posts/delete/' + postId)
+      .catch(function (error) { console.log(error); });
   }
 
   return (
@@ -34,15 +36,22 @@ function BubbleItem({ postId, title, author, about, link, likes }) {
             <div className="likes">{likes}</div>
           </div>
         </div>
-
         <div className="author">{author}</div>
         <div className="about">{about}</div>
 
       </div>
-      {link ?
-        <div className='linking'>
-          <a href={link} target="_blank" rel="noopener noreferrer">Sign Up</a>
-        </div> : null}
+
+      <div className='modalBottom'>
+        {link ?
+          <div className='linking'>
+            <a href={link} target="_blank" rel="noopener noreferrer">Sign Up</a>
+          </div> : null}
+        {
+          profile ?
+            <img src={Trash} alt='Delete Post' onClick={() => { deletePost(); }} /> : null
+        }
+      </div>
+
 
     </div>
 
@@ -50,4 +59,5 @@ function BubbleItem({ postId, title, author, about, link, likes }) {
 }
 
 export default BubbleItem;
+
 
